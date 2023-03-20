458ddc3c7aa1b5b6a26dc9e69ae07f6ead546dc2
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-20 19:34:31 +1300

    chore: change to meaningful response

--- backend/src/main/java/dev/jason/movies/MovieController.java
+++ backend/src/main/java/dev/jason/movies/MovieController.java
@@ -1,5 +1,7 @@
 package dev.jason.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
   @GetMapping
-  public String allMovies() {
-    return "All Movies!";
+  public ResponseEntity<String> allMovies() {
+    return new ResponseEntity<String>("All movies", HttpStatus.OK);
   }
 }
