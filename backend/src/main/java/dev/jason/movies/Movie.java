2bbc530ee59e1f38f1ec5d6ac04fdb8ca2aba215
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-20 18:58:52 +1300

    chore: add Movie class

---
+++ backend/src/main/java/dev/jason/movies/Movie.java
@@ -0,0 +1,27 @@
+package dev.jason.movies;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+  @Id
+  private ObjectId id;
+
+  private String imdbId;
+  private String title;
+  private String releaseDate;
+  private String trailerLink;
+  private String poster;
+  private List<String> genres;
+  private List<String> backdrops;
+}
